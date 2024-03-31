8a4c582593371c783ce06ba6dadc40acd7a832aa
DracoInferno <gemknight1997@gmail.com>
2024-03-31 15:08:18 -0700

    update

--- Assignment_4_Generate_Users/Keyword_User.py
+++ Assignment_4_Generate_Users/Keyword_User.py
@@ -5,7 +5,7 @@ import collections
 import csv
 
 def findKeyword(driver, keyword)->bool:
-    print(driver.page_source.lower())
+    #print(driver.page_source.lower())
     return keyword.lower() in driver.page_source.lower()
  
 def main():
@@ -15,7 +15,7 @@ def main():
     driver.get("http://localhost:3000/")
     reward_time = 10
     total_reward_time = 0
-    keywords = ["game", "anime"]
+    keywords = ["steven"]
     for key in keywords:
         if findKeyword(driver, key):
             total_reward_time += reward_time
--- Assignment_4_Generate_Users/Link_User.py
+++ Assignment_4_Generate_Users/Link_User.py
@@ -1,32 +1,56 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium import By
 
+def findText(driver, keyword):
+    if keyword.lower() in driver.page_source.lower():
+        return True
+    else:
+        return False
 
-def findKeyword(driver, keyword)->bool:
-    print(driver.page_source.lower())
-    return keyword.lower() in driver.page_source.lower()
-
-def countElem(driver, tag_name)->int:
-    return len(driver.find_elements(By.TAG_NAME, tag_name))
+def countTagElem(driver, tag_name)->int:
+    count = 0
+    for tag in tag_name:
+        count += len(driver.find_elements(By.TAG_NAME, tag))
+    return count
  
+def userAction(action, driver, reward_time, req_list)->float:
+    total_reward_time = 0
+    if action.upper() == "KEYWORD":
+            for keyword in req_list:
+                if findText(driver, keyword):
+                    print("found",keyword)
+                    time.sleep(reward_time)
+                    total_reward_time += reward_time
+                else:
+                    print("not found")
+    elif action.upper() == "IMAGE":
+        num_images = countTagElem(driver, req_list)
+        total_reward_time = reward_time*num_images
+        time.sleep(total_reward_time)
+    
+    return total_reward_time
+
 def clickLink(driver,href):
-    links = driver.find_elements
+     links = driver.find_elements(By.TAG_NAME, "a")
+
+     for link in links:
+         link.click() 
 
 def main():
+    # Initialize browser
     driver = webdriver.Chrome()
+    # Navigate to your website
     driver.get("http://localhost:3000/")
-    reward_time = 10
-    total_reward_time = userAction("KEYWORD", driver, reward_time, ["student", "test"])
-    tags_name = ["img"]
-
-    for tag in tags:
-        num_images = countElem(driver, tag)
-        total_reward_time += reward_time
-        time.sleep(reward_time)
+    reward_time = 10; 
+    total_reward_time = userAction("KEYWORD", driver, reward_time, ["game", "anime"])
+    tag_name = ["img"]
+    total_reward_time += userAction("IMAGE", driver, reward_time, tag_name)
+    #clickLink(driver)
     driver.quit()
-    print("Presence Time:", total_reward_time)
+
+    print("Presence Time",total_reward_time)
+
 if __name__ == "__main__":
     main()
     
